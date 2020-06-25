7db86a3f3f94c43e0dab3b745c7d2c9aff1132b0
Suhas1998 <suhas.edu23@gmail.com>
2020-06-25 23:15:32 +0530

    ContestId Input taken

--- sample.py
+++ sample.py
@@ -3,20 +3,31 @@ import os
 from os import listdir
 from os.path import dirname
 
+def getNumber(input_string):
+  pos = 0
+  number_string = ""
+  for i in input_string:
+    if i.isdigit():
+      number_string = number_string + i
+
+  return number_string
+
 class SampleCommand(sublime_plugin.TextCommand):
   def run(self, edit, action=None):
-    def on_done(input_string):
-      # self.view.run_command("move_to", {"to": "bof"})
-      # self.view.run_command("insert", {"characters": input_string})
-      print(input_string)
+    if action == None:
+      def on_done(input_string):
+        # self.view.run_command("move_to", {"to": "bof"})
+        # self.view.run_command("insert", {"characters": input_string})
+        contestId = int(getNumber(input_string))
+        print(contestId+1)
 
-    def on_change(input_string):
-      print("Input changed: %s" % input_string)
+      def on_change(input_string):
+        pass
 
-    def on_cancel():
-      print("User cancelled the input")
+      def on_cancel():
+        print("User cancelled the input")
 
-    window = self.view.window()
-    window.show_input_panel("Text to Insert:", "Hello, World!",on_done, on_change, on_cancel)
-    print("This Works")
+      window = self.view.window()
+      window.show_input_panel("Text to Insert:", "(Make Sure You Registered For The Contest) Enter Contest Id : ",on_done, on_change, on_cancel)
+      print("This Works")
 
